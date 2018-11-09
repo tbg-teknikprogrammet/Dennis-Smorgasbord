7c38f6cdd7b82bbf84c735e8883075918fcb216e
Dennis Lundberg <Dennis.Lundberg87@gmail.com>
2018-11-09 11:15:35 +0100

    completed function.js and started on array.js

--- projects/elev-javascript 101/assets/js/4-function.js
+++ projects/elev-javascript 101/assets/js/4-function.js
@@ -106,4 +106,159 @@ console.log("4 squared is: " + squared(4));
 // returnera den första return som den stöter på
 
 var result = squared(10);
-console.log(result);
+console.log(result);    
+
+/*
+***************************************
+functions - muntligt test
+***************************************
+*/
+
+// Test 1
+// Förklara vad som händer
+function test1(x, y) {
+    return y - x;
+}
+
+console.log(test1(10, -40));
+
+// Test 2
+// Förklara vad som händer
+function test2(x) {
+    return x * 2;
+    console.log(x);
+    return x / 2;
+}
+
+console.log(test2(40));
+
+/*
+***************************************function isEven() {
+
+}
+functions - skriftligt test
+***************************************
+*/
+
+// Write a function isEven() which takes a single numeric argument and 
+// returns true if the number is even, and false otherwise
+
+function isEven(num) {
+    if(num % 2 === 0) {
+        return true;
+    } 
+    else {
+        return false;
+    }
+}
+
+console.log(isEven(123));
+
+// Write a function kebabToSnake() which takes a single kebab-cased string
+// Argument and returns the snake_cased version
+// Basically replace "-" with "_"
+
+function kebabToSnake(str) {
+    // Everything inbetween / /g will get replaced - A pattern - A RegExp - Regular Expression
+    var myString = str.replace(/-/g, "_"); // replace makes a new copy of that string which we store in a new variable
+    return myString;
+}
+
+ kebabToSnake("Hej-jag-heter-Dennis");
+ console.log(kebabToSnake("h-e-l-l-o-W-o-r-l--d")); // helloWorld
+
+ /*
+***************************************
+JS Scope
+***************************************
+*/
+
+function localScope() {
+    var showScope = 31; 
+    console.log(showScope);
+}
+
+localScope();
+console.log(showScope);
+
+var showScope = "I'm a global variable"; // Global variable === Accessible in the entire script
+console.log(showScope);
+
+var showScopeY = 99; // Global variable
+function localScopeY() {
+    showScopeY = 100; 
+    console.log(showScopeY);
+}
+
+console.log(showScopeY); // Global showScopeY
+localScopeY();
+console.log(showScopeY); // Global showScopeY
+
+/*
+***************************************
+JS Scope - muntligt test
+***************************************
+*/
+
+// Förklara vad som händer
+var num = 8; // 1. global variable
+function doMath() { // 4. Script runs when invoked
+    num += 1; // 5. num is now 10
+    if(num % 5 === 0) { // 6. 10 / 5 === 2 === no modulo, Zero
+        return true; // 7. returns true
+    }
+    else {
+        return false;
+    }
+}
+
+num += 1; // 2. num is now 9
+doMath(); // 3. now the script runs
+
+function hi() {
+    var name = "Dennis"; // Local variable
+    console.log(name);
+}
+
+hi();
+
+function bye() {
+    console.log(name); // Variable not defined
+}
+
+bye();
+
+/*
+***************************************
+JS functions - higher order functions
+Simple explanation: A function that can take another function as an argument
+***************************************
+*/
+
+function sing() {
+    console.log("Im singing in the rain!");
+    console.log("Its funny on a sunny day!");
+}
+
+// setInterval(sing, 2500);
+
+// var anon = setInterval(function() {
+//     console.log("Im an anonymous function");
+// }, 3000);
+
+function myTimer() {
+    var d = new Date();
+    document.getElementById("output").innerHTML = d.toLocaleTimeString();
+}
+
+myTimer();
+
+var myTime = setInterval(myTimer, 1000);
+
+document.getElementsByTagName("button")[0].addEventListener("mouseenter", function() {
+    clearInterval(myTime);
+});
+
+document.querySelector("button").addEventListener("mouseleave", function() {
+    myTime = setInterval(myTimer, 1000);
+});
---
+++ projects/elev-javascript 101/assets/js/5-array.js
@@ -0,0 +1,8 @@
+// list a group of friends using var
+var friend1 = "Måns";
+var friend2 = "Sten";
+var friend3 = "Claes";
+var friend4 = "Måna";
+
+// list a group of friends using an array
+var friends = ["Måns", "Sten", "Claes", "Måna"]; // strings, numbers, booleans, objects
