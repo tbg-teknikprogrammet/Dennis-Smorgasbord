3dfc3a2c4664b2b0bcd5cc9182f5e9b48ade022d
Dennis Lundberg <Dennis.Lundberg87@gmail.com>
2018-11-29 14:04:48 +0100

    initialized ToDo app

---
+++ projects/ToDo/todo.js
@@ -0,0 +1,6 @@
+// Create a ToDo application
+
+// create an empty array
+// Add an event listener on button
+// as long as the input is not "quit" the app should continue running
+// use while() if() - elseif(), forEach, for(), push(), pop(), slice()
